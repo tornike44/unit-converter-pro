e461ba496aadeee71164b6e56666af59ef21fbb9
tornike44 <tornike.zar@gmail.com>
2025-05-30 23:05:21 +0200

    done first dashboard look

--- src/main/java/com/converter/service/conversion/AbstractConversionStrategy.java
+++ src/main/java/com/converter/service/conversion/AbstractConversionStrategy.java
@@ -1,55 +1,54 @@
 package com.converter.service.conversion;
 
-import com.converter.model.ConversionRequest;
-import com.converter.model.ConversionResult;
-import com.converter.model.ConversionType;
-
+import java.util.ArrayList;
 import java.util.Map;
 
+import com.converter.model.ConversionRequest;
+import com.converter.model.ConversionResult;
+
 public abstract class AbstractConversionStrategy implements ConversionStrategy {
-    
+
     protected abstract Map<String, Double> getFactors();
-    
+
     protected Double convertViaFactors(Double value, String from, String to) {
         Map<String, Double> factors = getFactors();
         Double fromFactor = factors.get(from.toLowerCase());
         Double toFactor = factors.get(to.toLowerCase());
-        
+
         if (fromFactor == null || toFactor == null) {
             throw new IllegalArgumentException("Unsupported unit conversion: " + from + " to " + to);
         }
-        
+
         Double valueInBase = value * fromFactor;
         return valueInBase / toFactor;
     }
-    
+
     protected Double convertSpecial(Double value, String from, String to) {
         throw new UnsupportedOperationException("Special conversion not implemented");
     }
-    
+
     protected boolean hasSpecialConversion(String from, String to) {
         return false;
     }
-    
+
     @Override
     public ConversionResult convert(ConversionRequest request) {
         try {
             Double result;
-            
+
             if (hasSpecialConversion(request.getFromUnit(), request.getToUnit())) {
                 result = convertSpecial(request.getValue(), request.getFromUnit(), request.getToUnit());
             } else {
                 result = convertViaFactors(request.getValue(), request.getFromUnit(), request.getToUnit());
             }
-            
+
             return new ConversionResult(
-                request.getValue(),
-                request.getFromUnit(),
-                result,
-                request.getToUnit(),
-                request.getType()
-            );
-            
+                    request.getValue(),
+                    request.getFromUnit(),
+                    result,
+                    request.getToUnit(),
+                    request.getType());
+
         } catch (Exception e) {
             ConversionResult error = new ConversionResult();
             error.setSuccess(false);
@@ -57,9 +56,14 @@ public abstract class AbstractConversionStrategy implements ConversionStrategy {
             return error;
         }
     }
-    
+
     @Override
     public boolean isRealTime() {
         return false;
+    }
+
+    @Override
+    public List<String> getAvailableUnits() {
+        return new ArrayList<>(getFactors().keySet());
     }
 }
--- src/main/java/com/converter/service/conversion/CurrencyConversionStrategy.java
+++ src/main/java/com/converter/service/conversion/CurrencyConversionStrategy.java
@@ -1,5 +1,7 @@
 package com.converter.service.conversion;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.Cacheable;
@@ -87,5 +89,13 @@ public class CurrencyConversionStrategy implements ConversionStrategy {
     @Override
     public boolean isRealTime() {
         return true;
+    }
+
+    @Override
+    public List<String> getAvailableUnits() {
+        return Arrays.asList(
+                "USD", "EUR", "GBP", "JPY", "AUD", "CAD", "CHF", "CNY",
+                "SEK", "NZD", "MXN", "SGD", "HKD", "NOK", "KRW", "TRY",
+                "RUB", "INR", "BRL", "ZAR");
     }
 }
